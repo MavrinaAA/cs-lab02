9ca1d9b4a8702a782d0ab8b5ee8a2b7f7317ce7c
Bob (MavrinaAA) <alenamavrina93@gmail.com>
2021-03-15 00:21:24 +0300

    code : min

--- main.cpp
+++ main.cpp
@@ -11,9 +11,16 @@ int main()
     cout << " A - B = " << a - b << '\n';
     cout << " A * B = " << a * b << '\n';
     cout << " A / B = " << a / b << '\n';
+
     if (a>b)
         cout<< a << '\n';
     else
         cout << b << '\n';
+
+    if (a < b)
+        cout << a;
+    else
+        cout << b; 
+
     return 0;
 }
